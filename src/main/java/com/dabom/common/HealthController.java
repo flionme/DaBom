6a8e922d1d55a8124e20b6ecfc35545ea6bf338a
flionme <sunginkim142@gmail.com>
2025-09-17 14:26:01 +0900

    test

---
+++ src/main/java/com/dabom/common/HealthController.java
@@ -0,0 +1,16 @@
+package com.dabom.common;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity health(){ return ResponseEntity.ok().build(); }
+
+}
